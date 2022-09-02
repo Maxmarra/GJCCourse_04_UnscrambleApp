172d5ca45f77a41a17df70d047c1e4c46fc60ab2
Maxmarra <5919669@gmail.com>
2022-09-02 16:32:14 +0300

    03_Update_score_and_word_count

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.SCORE_INCREASE
 import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -63,16 +64,38 @@ class GameViewModel : ViewModel() {
 
     fun checkUserGuess() {
 
-        if (userGuess.equals(currentWord, ignoreCase = true)) {
+        if (userGuess.trim().equals(currentWord, ignoreCase = true)) {
+                // User's guess is correct, increase the score
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
+            // Reset user guess
 
         } else {
             // Reset user guess
+            //чистим неправильное слово
             updateUserGuess("")
             // User's guess is wrong, show an error
             _uiState.update { currentState ->
                 currentState.copy(isGuessedWordWrong = true)
             }
         }
+    }
+    private fun updateGameState(updatedScore: Int) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                isGuessedWordWrong = false,
+                currentScrambledWord = pickRandomWordAndShuffle(),
+                score = updatedScore,
+                currentWordCount = currentState.currentWordCount.inc(),
+            )
+        }
+        //чистим отгаданное слово
+        updateUserGuess("")
+    }
 
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        //чистим если что-то было написано но все равно нажали "пропустить"
+        updateUserGuess("")
     }
 }
