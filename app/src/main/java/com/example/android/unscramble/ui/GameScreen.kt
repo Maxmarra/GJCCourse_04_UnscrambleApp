6decb2e85ed7e62a2542b195cf4ae90964b9bf2c
Maxmarra <5919669@gmail.com>
2022-09-02 11:02:18 +0300

    starter

--- app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (c)2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.unscramble.ui
 
 import android.app.Activity
@@ -114,7 +99,7 @@ fun GameLayout(modifier: Modifier = Modifier) {
 
         ) {
         Text(
-            text = "scrambleun",
+            text = "осн",
             fontSize = 45.sp,
             modifier = modifier.align(Alignment.CenterHorizontally)
         )
@@ -125,9 +110,9 @@ fun GameLayout(modifier: Modifier = Modifier) {
         )
         OutlinedTextField(
             value = "",
+            onValueChange = { },
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
-            onValueChange = { },
             label = { Text(stringResource(R.string.enter_your_word)) },
             isError = false,
             keyboardOptions = KeyboardOptions.Default.copy(
@@ -156,6 +141,7 @@ private fun FinalScoreDialog(
             // button. If you want to disable that functionality, simply use an empty
             // onCloseRequest.
         },
+
         title = { Text(stringResource(R.string.congratulations)) },
         text = { Text(stringResource(R.string.you_scored, 0)) },
         modifier = modifier,
